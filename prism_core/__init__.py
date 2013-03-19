c267605a62c23e6d4c8bfccd399604875af89b11
Elliot Peele <elliot@bentlogic.net>
2013-03-18 21:20:29 -0400

    remove autogenerated __init__

--- prism_core/__init__.py
+++ prism_core/__init__.py
@@ -1,20 +1,14 @@
-from pyramid.config import Configurator
-from sqlalchemy import engine_from_config
+#
+# Copyright (c) Elliot Peele <elliot@bentlogic.net>
+#
+# This program is distributed under the terms of the MIT License as found
+# in a file called LICENSE. If it is not present, the license
+# is always available at http://www.opensource.org/licenses/mit-license.php.
+#
+# This program is distributed in the hope that it will be useful, but
+# without any warrenty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the MIT License for full details.
+#
 
-from .models import (
-    DBSession,
-    Base,
-    )
-
-
-def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
-    engine = engine_from_config(settings, 'sqlalchemy.')
-    DBSession.configure(bind=engine)
-    Base.metadata.bind = engine
-    config = Configurator(settings=settings)
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.scan()
-    return config.make_wsgi_app()
+def includeme(config):
+    return config
